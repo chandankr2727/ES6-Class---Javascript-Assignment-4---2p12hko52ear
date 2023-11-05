f547cb037a915b597813e7c20ac044c91c8393e7
Chandan Kumar <88577612+chandankr2727@users.noreply.github.com>
2023-11-05 22:19:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-//Write your code here
+class API {
+  constructor(url, method = 'GET') {
+    this.url = url;
+    this.method = method;
+    this.#secure = url.startsWith('https');
+  }
 
+  isSecure() {
+    return this.#secure;
+  }
 
-
+  updateURL(newUrl) {
+    this.url = newUrl;
+    this.#secure = newUrl.startsWith('https');
+  }
+}
 
 
 //Do not remove this code
